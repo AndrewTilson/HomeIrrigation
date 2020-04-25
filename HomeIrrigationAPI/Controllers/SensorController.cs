e8a91f805183347cafe913cb4e0538e0ea2d0d23
Andrew Tilson <wadescomputer@msn.com>
2020-04-25 14:42:04 -0500

    Initial Commit

---
+++ HomeIrrigationAPI/Controllers/SensorController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HomeIrrigationAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+
+namespace HomeIrrigationAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SensorController : ControllerBase
+    {
+        public MySqlConnection Connection { get; }
+
+        public SensorController()
+        {
+            Connection = new MySqlConnection("Server=192.168.1.221:3306;User Id=svcIrrigationAPI;Password=Casper01Lucy;Database=IrrigationDB");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            await Connection.OpenAsync();
+            var query = new SensorReadingsModel(Connection);
+            var result = await query.GetAll();
+            return new OkObjectResult(result);
+        }
+
+        // POST: api/Sensor
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+    }
+}
---
+++ HomeIrrigationAPI/Models/SensorReadingsModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace HomeIrrigationAPI.Models
+{
+    public class SensorReadingsModel : IDisposable
+    {
+        public DateTime RecordedTS { get; set; }
+
+        public int ID { get; set; }
+
+        public int SensorID { get; set; }
+
+        public int Temp { get; set; }
+
+        public int Moisture { get; set; }
+
+        public MySqlConnection Connection { get; }
+
+        public SensorReadingsModel(MySqlConnection connection)
+        {
+            Connection = connection;
+        }
+
+        public async Task<string> GetAll()
+        {
+            var cmd = Connection.CreateCommand();
+            cmd.CommandText = "select count(*) SensorReadings";
+            var result = await cmd.ExecuteReaderAsync();
+
+            string results = "";
+            while (result.Read())
+            {
+                results = results + result.GetInt32(0);
+            }
+
+            return results;
+        }
+
+        public void Dispose() => Connection.Dispose();
+    }
+}
